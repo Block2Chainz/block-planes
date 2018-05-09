68b8a3e9591210536e638be5896d1ae817aede61
Joseph Nguyen <nguyen.josephduy@gmail.com>
2018-05-09 16:49:51 -0700

    compile id and attr of planes owned by user from blockchain

--- block-planes-client/src/components/collection/collection.jsx
+++ block-planes-client/src/components/collection/collection.jsx
@@ -10,6 +10,7 @@ class Collection extends Component {
     this.state = {
       account: '0x0',
       planes: [],
+      contract:''
     }
 
     if (typeof web3 != 'undefined') {
@@ -26,45 +27,62 @@ class Collection extends Component {
   }
 
   componentDidMount() {
-    // TODO: Refactor with promise chain
     this.web3.eth.getCoinbase((err, account) => {
       this.setState({ account })
       this.blockplanes.deployed().then((blockplanesInstance) => {
-        this.blockplanesInstance = blockplanesInstance;
-        console.log('planessss: ', this.blockplanesInstance, 'account', this.state.account)
-        
-      })
-    })
-
-    // this.getPlanesByOwner(this.state.account).then(this.addPlanes);
+        this.setState( {contract : blockplanesInstance} );
+        return blockplanesInstance;
+      }).then((contract) => {
+        this.setState({planes : this.state.contract.getPlanesByOwner(this.state.account)});
+        return contract.getPlanesByOwner(this.state.account);
+      }).then((planes) => {
+        let planeIds = [];
+        planes.forEach((plane) => {
+          planeIds.push(plane.toNumber());
+        });
+        return planeIds;
+      }).then((planeArray) => {
+        let hangar = [];
+        planeArray.forEach((planeId) => {
+          let planeAttr;
+          this.state.contract.planes(planeId).then((plane) => {
+             planeAttr = plane.toNumber();
+             hangar.push([planeId, planeAttr]);
+          });
+        })
+        console.log(hangar, 'flag1');          
+        return hangar;
+      }).then((finalArray) => {
+            console.log(finalArray, 'flag2');
+            this.setState({planes : finalArray});
+      }); 
+    });
   }
+    // this.setState({planes: this.state.planes.push([planeId, planeAttr])});
 
-  //Look up all planes by current user in contract
-  getPlanesByOwner(owner) {
-      return this.state.cryptoPlanes.methods.getPlanesByOwner(owner).call()
-  }  
-  
-  //Look up planes details in contract
-  getPlanesDetails(id) {
-      return cryptoPlanes.methods.planes(id).call()
-  }
-  
-  //add each plane to plane array in state
-  addPlanes(ids) {
-      var planeArray = [];
-      console.log('id in addPlane', ids);
-      ids.forEach = (id) => {
-        this.getPlaneDetails(id)
-        .then(function(plane) {
-          planeArray.concat(plane)} )
-      }
-      this.setState({planes: planeArray})
-    }
+  //TEST CODE CONTRACT COMMUNICATION
+      // this.web3.eth.getCoinbase((err, account) => {
+      //   this.setState({ account })
+      //   this.blockplanes.deployed().then((blockplanesInstance) => {
+      //     console.log('planessss: ', blockplanesInstance, 'account', this.state.account);
+      //     this.setState( {contract : blockplanesInstance} );
+      //     return blockplanesInstance;
+      //   }).then((contract) => {
+      //     // return contract.createRandomPlane();        
+      //     return contract.createRandomPlane({ from: this.web3.eth.accounts[0], value: this.web3.toWei(0.001, 'ether')});
+      //   }).then((receipt) => {
+      //     console.log('receipt: ', receipt, 'state: ', this.state);
+      //     return receipt.logs[0].args.planeId.toNumber();
+      //   }).then((planeCount) => {
+      //     console.log(this.state.contract.planeToOwner(planeCount));
+      //     return this.state.contract.planeToOwner(planeCount);
+      //   });
+      // });  
 
   render() {
     return (
       <div>
-        {this.state.planes}
+        {console.log('current state: ', this.state)}
       </div>
     )
   }
