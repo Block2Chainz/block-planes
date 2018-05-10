da0f9052bf0242d21be112beda3393f1417ebd1c
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-05-10 12:17:10 -0700

    hanger component

--- block-planes-client/src/actions/index.js
+++ block-planes-client/src/actions/index.js
@@ -3,4 +3,12 @@ import { LOG_OUT } from "../constants/action-types";
 
 export const logIn = user => ({ type: LOG_IN, payload: user });
 
-export const logOut = () => ({type: LOG_OUT});
+export const logOut = () => ({ type: LOG_OUT });
+
+export const storeContract = (contract) => ({ type: STORE_CONTRACT, payload: contract });
+
+export const storeUserAddress = (address) => ({ type: STORE_USER_ADDRESS, payload: address });
+
+export const storeUserPlanes = (planes) => ({ type: STORE_USER_PLANES, payload: planes });
+
+
