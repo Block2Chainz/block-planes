e626121b058c704d842978c4be84d935a140393f
Joseph Nguyen <nguyen.josephduy@gmail.com>
2018-05-14 10:09:15 -0700

    query db for top scores, top score table use top score query

--- block-planes-api/controllers/leaderboard.js
+++ block-planes-api/controllers/leaderboard.js
@@ -13,36 +13,43 @@ const getHiScore = (req, res) => {
   db.query('SELECT username, id, profile_picture FROM users', (err, data) => {
     for (let i = 0; i < data.length; i++) {
       // console.log('id: ', data[i].id, 'username: ', data[i].username);
-      client.zadd('leaderboard', data[i].id, data[i].username + '___' + data[i].profile_picture);
+      client.zadd('leaderboardHi', data[i].id, data[i].username + '___' + data[i].profile_picture);
     }
     // console.log('data from db: ', data)
     
-    client.zrevrange('leaderboard', 0, 9, 'withscores', function(err, scores){
+    client.zrevrange('leaderboardHi', 0, 9, 'withscores', function(error, scores){
       console.log('scores: ', scores);
       let highScores = [];
       while (scores.length) {
-        let tup = scores.splice(0,2)
+        let tup = scores.splice(0,2);
         // console.log('tup: ', tup, 'scores: ', scores)
         highScores.push({name: tup[0].split('___')[0], picture: tup[0].split('___')[1], score: tup[1]});
-        console.log('highScores array: ', highScores)
+        console.log('highScores array: ', highScores);
       }
 
       res.send(highScores);
     });
-  })
+  });
+};
 
-  // .then(function() {
-  //   console.log('highScores array: ', highScores)
-  // })
-  
-  // return highScores;
-// console.log('flag1', highScores)
-// res.send(highScores);
-}
+const getTotal = (req, res) => {
+  db.query('SELECT username, id, profile_picture FROM users', (err, data) => {
+    for (let i = 0; i < data.length; i++) {
+      client.zadd('leaderboardTotal', data[i].id, data[i].username + '___' + data[i].profile_picture);
+    }
+    
+    client.zrevrange('leaderboardTotal', 0, 9, 'withscores', function(error, scores){
+      let topScores = [];
+      while (scores.length) {
+        let tup = scores.splice(0,2);
+        topScores.push({name: tup[0].split('___')[0], picture: tup[0].split('___')[1], score: tup[1]});
+        console.log('topScores array: ', topScores);
+      }
 
-const getTotal = () => {
-
-}
+      res.send(topScores);
+    });
+  });
+};
 
 module.exports.getHiScore = getHiScore;
 module.exports.getTotal = getTotal;
--- block-planes-client/src/components/highScore/HighScores.jsx
+++ block-planes-client/src/components/highScore/HighScores.jsx
@@ -11,18 +11,19 @@ class HighScores extends Component {
         super(props);
         this.state = {
           highScores: [],
+          topScores: []
         };
     }
 
     componentWillMount() {
         this.getHighScores();
+        this.getTopScores();
     }
 
     getHighScores() {
       axios
       .get(`/leaderboardhi`)
       .then(response => {
-        console.log('flag1: ', response.data);
         this.setState({highScores : response.data});
       })
       .catch(err => {
@@ -30,17 +31,32 @@ class HighScores extends Component {
       });
     }
 
+    getTopScores() {
+      axios
+      .get(`/leaderboardTotal`)
+      .then(response => {
+        this.setState({topScores : response.data});
+      })
+      .catch(err => {
+        console.log('Error from get top scores', err);
+      });
+    }
+
     render() {
         let title;
         let title2;
         let title3;
-        let cellColor;        
+        let cellColor;
+        let title4;
+        let title5;
+        let title6;
         return (
           <div>
+            {console.log('leaderboard state: ', this.state)}
             <div>
               <p className="page-title">LEADERBOARD</p>
             </div>
-          {/* ********FIRST TABLE******** */}
+          {/* ********HIGH SCORE TABLE******** */}
           <div class="half-width">
             <div className="table-title">
               <p >Weekly High Score</p>
@@ -70,7 +86,6 @@ class HighScores extends Component {
                     <Image src={set.picture} rounded size='mini' />
                     <Header.Content className="cell-player-header">
                         {set.name}
-                      {/* <Header.Subheader>Cool Player</Header.Subheader> */}
                     </Header.Content>
                   </Header>
                 </Table.Cell>
@@ -85,7 +100,7 @@ class HighScores extends Component {
           </div>
         </div>
 
-        {/* ********SECOND TABLE******** */}
+        {/* ********TOP SCORE TABLE******** */}
         <div class="half-width">
         <div className="table-title">
           <p >Weekly Top Score</p>
@@ -96,30 +111,29 @@ class HighScores extends Component {
           <Table.Row>
             <Table.HeaderCell className="table-header">Rank</Table.HeaderCell>
             <Table.HeaderCell className="table-header">Player</Table.HeaderCell>
-            <Table.HeaderCell className="table-header">High Score</Table.HeaderCell>
+            <Table.HeaderCell className="table-header">Top Score</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
       <Table.Body>
-        {this.state.highScores.map((set, index) => {
-          (index === (this.state.highScores.length - 1)) ? (title = 'cell-player-bottom') : (title = 'cell-player');
-          (index === (this.state.highScores.length - 1)) ? (title2 = 'cell-score-bottom') : (title2 = 'cell-score'); 
-          (index === (this.state.highScores.length - 1)) ? (title3 = 'cell-rank-bottom') : (title3 = 'cell-rank');
+        {this.state.topScores.map((set, index) => {
+          (index === (this.state.topScores.length - 1)) ? (title4 = 'cell-player-bottom') : (title4 = 'cell-player');
+          (index === (this.state.topScores.length - 1)) ? (title5 = 'cell-score-bottom') : (title5 = 'cell-score'); 
+          (index === (this.state.topScores.length - 1)) ? (title6 = 'cell-rank-bottom') : (title6 = 'cell-rank');
           (index % 2 === 0) ? (cellColor = 'cell-color-gray') : (cellColor = 'cell-color-black');                                          
           return (
             <Table.Row >
-                <Table.Cell className={`${title3} ${cellColor}`}>
+                <Table.Cell className={`${title6} ${cellColor}`}>
                 {index +  1}
               </Table.Cell>
-            <Table.Cell className={`${title} ${cellColor}`}>
+            <Table.Cell className={`${title4} ${cellColor}`}>
               <Header as='h4' image>
                 <Image src={set.picture} rounded size='mini' />
                 <Header.Content className="cell-player-header">
                     {set.name}
-                  {/* <Header.Subheader>Cool Player</Header.Subheader> */}
                 </Header.Content>
               </Header>
             </Table.Cell>
-            <Table.Cell className={`${title2} ${cellColor}`}>
+            <Table.Cell className={`${title5} ${cellColor}`}>
                 {set.score}
             </Table.Cell>
           </Table.Row>);
