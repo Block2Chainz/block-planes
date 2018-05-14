dae54faba19e89a7531b04c03383cb10b5baab2a
Joseph Nguyen <nguyen.josephduy@gmail.com>
2018-05-14 16:07:26 -0700

    set cron job to query db every Monday at 1230 am

--- block-planes-api/controllers/leaderboard.js
+++ block-planes-api/controllers/leaderboard.js
@@ -8,33 +8,30 @@ client.on('connect', function() {
   console.log('connected to redis client at leaderboard');
 });
 
-const pullData = new CronJob ( '30 * * * * 1-5', function() {
-    db.query('SELECT username, id, profile_picture FROM users', (err, data) => {
-      for (let i = 0; i < data.length; i++) {
-        // console.log('id: ', data[i].id, 'username: ', data[i].username);
-        client.zadd('leaderboardHi', data[i].id, data[i].username + '___' + data[i].profile_picture);
-        client.zadd('leaderboardTotal', data[i].id, data[i].username + '___' + data[i].profile_picture);
-      }
-      console.log('inside cron job: ', data, 'time', Date.now());  
-    });
+const pullData = new CronJob ( '* 30 00 * * 1', function() {
+    queryDb();
   },
   null,
   true,
   'America/Los_Angeles'
 );
 
+const queryDb = () => {
+  db.query('SELECT username, id, profile_picture FROM users', (err, data) => {
+    for (let i = 0; i < data.length; i++) {
+      client.zadd('leaderboardHi', data[i].id, data[i].username + '___' + data[i].profile_picture);
+      client.zadd('leaderboardTotal', data[i].id, data[i].username + '___' + data[i].profile_picture);
+    }
+  });
+};
 
 
 const getHiScore = (req, res) => {
-  client.del('leaderboardHi');
   client.zrevrange('leaderboardHi', 0, 9, 'withscores', function(error, scores){
-    console.log('scores: ', scores);
     let highScores = [];
     while (scores.length) {
       let tup = scores.splice(0,2);
-      // console.log('tup: ', tup, 'scores: ', scores)
       highScores.push({name: tup[0].split('___')[0], picture: tup[0].split('___')[1], score: tup[1]});
-      console.log('highScores array: ', highScores);
     }
 
     res.send(highScores);
@@ -42,13 +39,11 @@ const getHiScore = (req, res) => {
 };
 
 const getTotal = (req, res) => {
-  client.del('leaderboardTotal');
   client.zrevrange('leaderboardTotal', 0, 9, 'withscores', function(error, scores){
     let topScores = [];
     while (scores.length) {
       let tup = scores.splice(0,2);
       topScores.push({name: tup[0].split('___')[0], picture: tup[0].split('___')[1], score: tup[1]});
-      console.log('topScores array: ', topScores);
     }
     res.send(topScores);
   });
