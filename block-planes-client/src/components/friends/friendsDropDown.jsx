256cacbb67411a00461764b424cde0878d128282
Joseph Nguyen <nguyen.josephduy@gmail.com>
2018-05-24 10:32:37 -0700

    adjust select friend drop down height

--- block-planes-client/src/components/friends/friendsDropDown.jsx
+++ block-planes-client/src/components/friends/friendsDropDown.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
 const FriendsDropDown = (props) => (
-  <Dropdown placeholder='Select Friend' search selection options={props.friends} onChange={(e, data) => props.updateFriendsPage(data.options[data.value])}/>
+  <Dropdown className='dropdown-friends' placeholder='Select Friend' search selection options={props.friends} onChange={(e, data) => props.updateFriendsPage(data.options[data.value])}/>
 )
 
 export default FriendsDropDown;
