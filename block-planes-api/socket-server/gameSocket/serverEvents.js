0641d124964e40e0a6ed83f85f96df6cea186976
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-05-23 19:28:29 -0700

    restart game working

--- block-planes-api/socket-server/gameSocket/serverEvents.js
+++ block-planes-api/socket-server/gameSocket/serverEvents.js
@@ -10,15 +10,6 @@ module.exports.serverp2Ready = ({ io, client, room, player }, netPeer ) => {
     io.sockets.in(room.id).emit('p2_ready', netPeer);
 };
 
-module.exports.serverUpdate = ({ client, room }) => {
-    io.sockets.in(room.id).emit('update', { bullets: room.get('bullets'), 
-                            particles: room.get('particles'),
-                            p1_ship: room.get('p1_ship'), 
-                            p2_ship: room.get('p2_ship'),
-                            enemies: room.get('enemies'),                     
-                        });
-};
-
 module.exports.serverShipGeneration = ({ client, room, player }, payload) => {
     if (payload.ship1) {
         client.emit('ship1', payload.ship1);
